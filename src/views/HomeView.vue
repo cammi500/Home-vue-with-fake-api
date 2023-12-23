b1b44d43cca2a4daa8eaaa84c1da1f37c5c4d777
THIRI AUNG <ss.sonali2003@gmail.com>
2023-12-23 13:25:08 +0630

    after clicking data work

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,5 +1,5 @@
 <template>
-  <div>
+  <div class="container">
     <h1>Home</h1>
 
     <button class="btn btn-danger my-5" @click="getData()">click</button>
@@ -8,6 +8,30 @@
       <h1>data have</h1>
     </div>
 
+    <table class="table table-striped">
+      <thead>
+        <tr>
+          <th scope="col">ID</th>
+          <th scope="col">Category</th>
+          <th scope="col">Des</th>
+          <th scope="col">Image</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr v-if="posts.length ==0">
+          <th scope="row" colspan="4" >There i no data</th>
+        </tr>
+        <tr v-else v-for="(p,index) in posts" :key="index">
+          <th scope="row">{{ p.id }}</th>
+          <td>{{p.category}}</td>
+          <td>{{p.price}}</td>
+          <td>
+            <img :src="p.image" width="100" height="100"/>
+          </td>
+        </tr>
+       
+      </tbody>
+    </table>
   </div>
 </template>
 
@@ -17,6 +41,7 @@
     data() {
       return {
         loadingStatus : false,
+        posts: [],
       }
     },
     methods : {
@@ -24,8 +49,9 @@
         this.loadingStatus = true;
       fetch('https://fakestoreapi.com/products')
             .then(res=>res.json())
-            .then(json=>{console.log(json)
-            this.loadingStatus = false;
+            .then((json) =>{
+              this.posts = json;
+              this.loadingStatus = false;
             });
     }
     }
